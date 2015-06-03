d2531550dcfa2a3eef662a7f76a3c023d81439bb
nmadariaga <tali-tali@openmailbox.org>
2015-06-03 17:05:38 -0300

    Cambios:Borro duplicados

--- app/Facultades.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Facultades extends Model {
-	protected $table = 'facultades';
-	protected $fillable = ['nombre','direccion','campus_id','descripcion'];
-	protected $guarded = ['id'];
-}
